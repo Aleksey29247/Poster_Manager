3403c60eb9ed6160ab7b1149acff08c924d258b9
Aleksey <Alik29247@gmail.com>
2022-03-05 19:59:43 +0700

    Update Manager
    Signed-off-by: Aleksey <Alik29247@gmail.com>

--- src/main/java/ru/netology/Manager.java
+++ src/main/java/ru/netology/Manager.java
@@ -2,30 +2,30 @@ package ru.netology;
 
 public class Manager {
     private Cinema[] cinema;
-    private int idMax;
+    private int limitedRecord;
     private int id;
 
     public Manager() {
         this.cinema = new Cinema[10];
-        this.idMax = 10;
+        this.limitedRecord = 10;
         this.id = 0;
     }
 
-    public Manager(int idMax) {
-        this.idMax = idMax;
-        this.cinema = new Cinema[this.idMax];
+    public Manager(int limitedRecord) {
+        this.limitedRecord = limitedRecord;
+        this.cinema = new Cinema[this.limitedRecord];
         this.id = 0;
     }
 
     public void addCinema(Cinema obj) {
-        if (id < idMax) {
+        if (id < limitedRecord) {
             cinema[id] = obj;
             id++;
         }
     }
 
     public Cinema[] getAll() {
-        for (int i = 0; i < idMax; i++) {
+        for (int i = 0; i < limitedRecord; i++) {
             if (cinema[i] == null) {
                 Cinema[] tmp = new Cinema[i];
                 for (int count = 0; count < i; count++) {
@@ -47,20 +47,21 @@ public class Manager {
     }
 
     public Cinema[] findLast() {
-        int lessMax = 0;
-        if (idMax >= 10)
-            lessMax = 10;
-        else
-            lessMax = idMax;
+        int LimitedMax = 0;
+        if (limitedRecord >= 10) {
+            LimitedMax = 10;
+        } else {
+            LimitedMax = limitedRecord;
+        }
         Cinema[] tmp = new Cinema[id];
         for (int i = 0; i < id; i++) {
             int index = cinema.length - i - 1;
-            if (i <= lessMax) {
+            if (i <= LimitedMax) {
                 tmp[i] = cinema[index];
 
             } else {
-                Cinema[] tmp1 = new Cinema[lessMax];
-                for (int j = 0; j <= lessMax - 1; j++) {
+                Cinema[] tmp1 = new Cinema[LimitedMax];
+                for (int j = 0; j <= LimitedMax - 1; j++) {
                     tmp1[j] = tmp[j];
                 }
                 return tmp1;
